a5f8612a5f8078e6c7566addbc8ea16db03ef6f7
Rosenheim <patrickrosenheim@gmail.com>
2016-05-19 13:24:38 +0200

    Connection testing.

--- app/src/main/java/app/ciscoconfig/dk/ciscoconfigapp/MainActivity.java
+++ app/src/main/java/app/ciscoconfig/dk/ciscoconfigapp/MainActivity.java
@@ -1,23 +1,20 @@
 package app.ciscoconfig.dk.ciscoconfigapp;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.net.Socket;
-import java.net.UnknownHostException;
 
 public class MainActivity extends AppCompatActivity {
 
     //Variabler på Main Activity
-    Intent  intent1;
+    Intent intent1;
     private EditText EditIpAddress = null;
     private EditText EditPortNr = null;
     private Button BtnConnect;
@@ -28,18 +25,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //Fundet komponenterne via ID.
-        EditIpAddress = (EditText)findViewById(R.id.EditIPAdresse);
-        EditPortNr = (EditText)findViewById(R.id.EditPortNr);
-        BtnConnect = (Button)findViewById(R.id.BtnConnecting);
-        BtnConnect.setOnClickListener(new View.OnClickListener()
-        {
+        EditIpAddress = (EditText) findViewById(R.id.EditIPAdresse);
+        EditPortNr = (EditText) findViewById(R.id.EditPortNr);
+        BtnConnect = (Button) findViewById(R.id.BtnConnecting);
+        BtnConnect.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                if (EditIpAddress != null || !EditIpAddress.getText().equals("") && EditPortNr != null || !EditPortNr.getText().equals(""))
-                {
-
-                try {
+                if (EditIpAddress.getText().toString().equals("") || EditPortNr.getText().toString().equals("")) {
+                    Toast.makeText(getApplicationContext(), R.string.connect_noEntry, Toast.LENGTH_SHORT).show();
+                } else {
+                    try {
                         String IpAddress = EditIpAddress.getText().toString();
                         String PortNr = EditPortNr.getText().toString();
 
@@ -49,20 +44,14 @@ public class MainActivity extends AppCompatActivity {
                         if (Sock.isConnected()) {
                             intent1 = new Intent(MainActivity.this, HomeActivity.class);
                             startActivity(intent1);
-                            Toast.makeText(getApplicationContext(), "Connected", Toast.LENGTH_LONG).show();
+                            Toast.makeText(getApplicationContext(), R.string.connect_Connected, Toast.LENGTH_LONG).show();
                         }
-
-                    }catch(IOException e){
+                    } catch (IOException e) {
                         e.printStackTrace();
                     }
-                }else
-                {
-                    Toast.makeText(getApplicationContext(), "IpAddress & Port Number is Empty or Wrong.", Toast.LENGTH_SHORT);
                 }
             }
         });
-
     }
-
 }
 
