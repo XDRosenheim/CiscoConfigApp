89d7937c73213469fe52b6c8a435be55005273fb
Rosenheim <patrickrosenheim@gmail.com>
2016-05-20 09:33:30 +0200

    IP check as a function.

--- app/src/main/java/app/ciscoconfig/dk/ciscoconfigapp/MainActivity.java
+++ app/src/main/java/app/ciscoconfig/dk/ciscoconfigapp/MainActivity.java
@@ -31,35 +31,40 @@ public class MainActivity extends AppCompatActivity {
         BtnConnect.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (EditIpAddress.getText().toString().equals("") || EditPortNr.getText().toString().equals("")) {
-                    Toast.makeText(getApplicationContext(), R.string.connect_noEntry, Toast.LENGTH_SHORT).show();
-                } else {
-                    try {
-                        String IpAddress = EditIpAddress.getText().toString();
-                        String PortNr = EditPortNr.getText().toString();
-                        Toast.makeText(getApplicationContext(), R.string.connect_connecting, Toast.LENGTH_LONG).show();
-                        //Opretter en socket som bruges som Telnet til at connecte til Router.
-                        Socket Sock = new Socket(IpAddress, Integer.parseInt(PortNr));
-                        if (Sock.isConnected()) {
-                            intent1 = new Intent(MainActivity.this, HomeActivity.class);
-                            startActivity(intent1);
-                            Toast.makeText(getApplicationContext(), R.string.connect_Connected, Toast.LENGTH_LONG).show();
-                        }
-                    } catch (IOException e) {
-                        e.printStackTrace();
+            if (checkIPPort(EditIpAddress.getText().toString(), Integer.parseInt(EditPortNr.getText().toString()))) {
+                Toast.makeText(getApplicationContext(), R.string.connect_noEntry, Toast.LENGTH_SHORT).show();
+            } else {
+                try {
+                    String IpAddress = EditIpAddress.getText().toString();
+                    String PortNr = EditPortNr.getText().toString();
+                    Toast.makeText(getApplicationContext(), R.string.connect_connecting, Toast.LENGTH_LONG).show();
+                    //Opretter en socket som bruges som Telnet til at connecte til Router.
+                    Socket Sock = new Socket(IpAddress, Integer.parseInt(PortNr));
+                    if (Sock.isConnected()) {
+                        intent1 = new Intent(MainActivity.this, HomeActivity.class);
+                        startActivity(intent1);
+                        Toast.makeText(getApplicationContext(), R.string.connect_Connected, Toast.LENGTH_LONG).show();
                     }
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
+            }
             }
         });
     }
 
     private boolean checkIPPort(String ip, int port) {
         try {
-            String[] parts = ip.split( "\\." );
-            for (String s: parts) {
-                //Do stuff
+            if (ip == null || ip.length() != 4) return false;
+            String[] parts = ip.split("\\.");
+            for (String s : parts) {
+                int i = Integer.parseInt(s);
+                if ((i < 1) || (i > 255)) {
+                    return false;
+                }
             }
-        } catch(NumberFormatException nfe) {
+            return true;
+        } catch (NumberFormatException nfe) {
             return false;
         }
     }
