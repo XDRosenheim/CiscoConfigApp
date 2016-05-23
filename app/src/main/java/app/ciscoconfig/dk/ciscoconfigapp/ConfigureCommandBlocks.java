197c01ba1553df36d5cf39c7d14419ebf699e0a9
Tor Soya <torsoya@gmail.com>
2016-05-23 13:11:38 +0200

    Added more Blocks

--- app/src/main/java/app/ciscoconfig/dk/ciscoconfigapp/ConfigureCommandBlocks.java
+++ app/src/main/java/app/ciscoconfig/dk/ciscoconfigapp/ConfigureCommandBlocks.java
@@ -11,9 +11,29 @@ public class ConfigureCommandBlocks {
 
     public ArrayList<String> array;
 
-    //Configure Serieal.
-    
+    //configure Rip Protocol // not done yet
+    public ArrayList configureRIPv2(String SerialInterface){
+        array.clear();
+        array.add("Version 2");
+        array.add("No Auto-Summary");
+        array.add("passive interface "+ SerialInterface);
+        return array;
+    }
 
+    //Configure Serieal interface.
+    public ArrayList setSerialInterface(String Clock ,String Interface,String Description, String ip, String subnetmask){
+        array.clear();
+        array.add("conf t");
+        array.add("int serial "+Interface);
+        if (Description.toString() == ""){}
+        else{array.add("description "+Description);}
+        array.add("ip address "+ip+" "+subnetmask);
+        if (Clock.toString() == ""){}
+        else{array.add("clock rate "+Clock);}
+        array.add("no shut");
+        array.add("end");
+        return array;
+    }
 
     //no ip domain-lookup
     public ArrayList noIpDomainLookUp(){
