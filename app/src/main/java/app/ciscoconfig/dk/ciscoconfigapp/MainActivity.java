eee5f7fd47ce95ee4e64a034187cddfa91dce5a7
Rosenheim <patrickrosenheim@gmail.com>
2016-05-24 10:46:16 +0200

    WE HAVE CONNECTION!

--- app/src/main/java/app/ciscoconfig/dk/ciscoconfigapp/MainActivity.java
+++ app/src/main/java/app/ciscoconfig/dk/ciscoconfigapp/MainActivity.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.renderscript.Script;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -30,6 +32,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
         //Fundet komponenterne via ID.
         EditIpAddress = (EditText) findViewById(R.id.EditIPAdresse);
         EditPortNr = (EditText) findViewById(R.id.EditPortNr);
@@ -46,7 +51,6 @@ public class MainActivity extends AppCompatActivity {
                         //Opretter en socket som bruges som Telnet til at connecte til Router.
                         Socket Sock = new Socket(IpAddress, Integer.parseInt(PortNr));
                         Sock.setKeepAlive(true);
-                        Sock.connect(Sock.getRemoteSocketAddress(), 100);
                         if (Sock.isConnected()) {
                             ToConf = new Intent(MainActivity.this, HomeActivity.class);
                             startActivity(ToConf);
