7e78418da2816ac813aa6e606b2cf6942ebfbb9c
Tor Soya <torsoya@gmail.com>
2016-05-26 10:55:19 +0200

    Still not working proberly
    it seams that the String arrays i retruning null, can figure out why

--- app/src/main/java/app/ciscoconfig/dk/ciscoconfigapp/CiscoDevices.java
+++ app/src/main/java/app/ciscoconfig/dk/ciscoconfigapp/CiscoDevices.java
@@ -1,5 +1,6 @@
 package app.ciscoconfig.dk.ciscoconfigapp;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,25 +10,25 @@ import java.util.List;
  */
 public class CiscoDevices {
 
-    List<String[]> DeviceInfo;
-    public String[] DeviceModel;
-    public String[] FastEthernetInterfaces;
-    public String[] GigabitInterfaces;
-    public String[] SerialInterfaces;
+    public String[][] DeviceInfo;
+    //public String[][] DeviceModel = new String[][];
+    public String[][] FastEthernetInterfaces;
+    public String[][] GigabitInterfaces;
+    public String[][] SerialInterfaces;
 
-    public ArrayList Router1900(){
-        DeviceModel = new String[]{"Router 1900"};
+    public String[][] Router1900(){
         for (int i=0;i<1;i++) {
-            GigabitInterfaces = new String[]{"GigabitEthernet 0/"+i};
+            //DeviceInfo[1] = new String[][]{"Gigabitethernet 0/"+i};
+            GigabitInterfaces[i] = new String[]{"Gigabitethernet 0/"+i};
         }
         for (int i=0;i<1;i++) {
-            SerialInterfaces = new String[]{"Serial 0/0/"+i};
+            //DeviceInfo[2] = new String[][]{"Serial 0/"+i};
+            SerialInterfaces[i] = new String[]{"Serial 0/0/"+i};
         }
+        DeviceInfo[0] = new String[]{"Router 1900"};
+        DeviceInfo[1] = new String[]{GigabitInterfaces.toString()};
+        DeviceInfo[2] = new String[]{SerialInterfaces.toString()};
 
-        DeviceInfo.add(DeviceModel);
-        DeviceInfo.add(GigabitInterfaces);
-        DeviceInfo.add(SerialInterfaces);
-        return (ArrayList) DeviceInfo;
+        return DeviceInfo;
     }
-
 }
