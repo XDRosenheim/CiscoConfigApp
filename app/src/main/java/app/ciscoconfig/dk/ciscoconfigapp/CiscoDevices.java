52d7db9abcc21422f83f5ad8a8b6fcb09dabb600
Tor Soya <torsoya@gmail.com>
2016-05-27 14:17:36 +0200

    down grade to 1 dimensonal array

--- app/src/main/java/app/ciscoconfig/dk/ciscoconfigapp/CiscoDevices.java
+++ app/src/main/java/app/ciscoconfig/dk/ciscoconfigapp/CiscoDevices.java
@@ -10,24 +10,24 @@ import java.util.List;
  */
 public class CiscoDevices {
 
-    public String[][] DeviceInfo;
+    public String[] DeviceInfo;
     //public String[][] DeviceModel = new String[][];
-    public String[][] FastEthernetInterfaces;
-    public String[][] GigabitInterfaces;
-    public String[][] SerialInterfaces;
+    public String[] FastEthernetInterfaces;
+    public String[] GigabitInterfaces;
+    public String[] SerialInterfaces;
 
-    public String[][] Router1900(){
+    public String[] Router1900(){
         for (int i=0;i<1;i++) {
             //DeviceInfo[1] = new String[][]{"Gigabitethernet 0/"+i};
-            GigabitInterfaces[i] = new String[]{"Gigabitethernet 0/"+i};
+            GigabitInterfaces[i] = "Gigabitethernet 0/"+i;
         }
         for (int i=0;i<1;i++) {
             //DeviceInfo[2] = new String[][]{"Serial 0/"+i};
-            SerialInterfaces[i] = new String[]{"Serial 0/0/"+i};
+            SerialInterfaces[i] = "Serial 0/0/"+i;
         }
-        DeviceInfo[0] = new String[]{"Router 1900"};
-        DeviceInfo[1] = new String[]{GigabitInterfaces.toString()};
-        DeviceInfo[2] = new String[]{SerialInterfaces.toString()};
+        DeviceInfo[0] = "Router 1900";
+        DeviceInfo[1] = GigabitInterfaces.toString();
+        DeviceInfo[2] = SerialInterfaces.toString();
 
         return DeviceInfo;
     }
