13953b82f8b67a1b4ac788fa1a7af9b342552d3b
Tor Soya <torsoya@gmail.com>
2016-05-31 11:55:30 +0200

    commands write out
    1.Writing out hte commands there is send to the  device in the Textview.
    2.Added auto scrolldown to the textview showing the commands send.
    
    TO Do:
    showing the line in's so the user can see that the wished config has
    been applied.

--- app/src/main/java/app/ciscoconfig/dk/ciscoconfigapp/HomeActivity.java
+++ app/src/main/java/app/ciscoconfig/dk/ciscoconfigapp/HomeActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 import java.io.BufferedReader;
@@ -16,9 +17,12 @@ public class HomeActivity extends AppCompatActivity {
 
     Button btnBack, btnSetHostname, btnSetMotd;
     TextView txtConsole;
+    ScrollView viewterminal;
     private Socket Sock;
     private PrintWriter Out;
     private BufferedReader In;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,55 +33,80 @@ public class HomeActivity extends AppCompatActivity {
         Out = Singleton.getOut();
         In = Singleton.getIn();
 
+        btnSetHostname = (Button) findViewById(R.id.btnSetHostname);
+        btnSetMotd = (Button) findViewById(R.id.btnSetMotd);
+        btnBack = (Button) findViewById(R.id.btnBack);
+
+        viewterminal = (ScrollView) findViewById(R.id.viewterminal);
+
         txtConsole = (TextView) findViewById(R.id.txtConsole);
 
         Thread t1 = new Thread(new Runnable() {
             @Override
             public void run() {
+
                 try {
-                    String reader = In.readLine();
+                    String reader;
+                    String newText = "";
+                    reader = In.readLine();
+                    newText += txtConsole.getText().toString()+"\n"+reader.toString();
                     if ( reader != null ) {
-                        txtConsole.append(reader);
+                        txtConsole.setText(newText);
+                        viewterminal.fullScroll(View.FOCUS_DOWN);
                     }
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
         });
+        Thread t2 = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+
+                String newText;
+
+                btnSetHostname.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        ConfigureCommandBlocks cmd = new ConfigureCommandBlocks();
+                        cmd.setHostName("ThisIsMyLifeNow");
+                        for (String sendMe : cmd.array) {
+                            Out.println(sendMe); // Send commands to device.
+                            txtConsole.setText(txtConsole.getText().toString()+"\n"+sendMe);
+                            viewterminal.fullScroll(View.FOCUS_DOWN);
+                        }
+                    }
+                });
+
+
+                btnSetMotd.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        ConfigureCommandBlocks cmd = new ConfigureCommandBlocks();
+                        cmd.setMOTD("This is your brand new MOTD!");
+                        for (String sendMe : cmd.array) {
+                            Out.println(sendMe); // Send commands to device.
+                            txtConsole.setText(txtConsole.getText().toString()+"\n"+sendMe);
+                            viewterminal.fullScroll(View.FOCUS_DOWN);
+                        }
+                    }
+                });
+
+
+                btnBack.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        CloseAll();
+                        finish(); // Die.
+                    }
+                });
+
+            }
+        });
+
+        t2.start();
         t1.start();
-
-        btnSetHostname = (Button) findViewById(R.id.btnSetHostname);
-        btnSetHostname.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ConfigureCommandBlocks cmd = new ConfigureCommandBlocks();
-                cmd.setHostName("ThisIsMyLifeNow");
-                for (String sendMe : cmd.array) {
-                    Out.println(sendMe); // Send commands to device.
-                }
-            }
-        });
-
-        btnSetMotd = (Button) findViewById(R.id.btnSetMotd);
-        btnSetMotd.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ConfigureCommandBlocks cmd = new ConfigureCommandBlocks();
-                cmd.setMOTD("This is your brand new MOTD!");
-                for (String sendMe : cmd.array) {
-                    Out.println(sendMe); // Send commands to device.
-                }
-            }
-        });
-
-        btnBack = (Button) findViewById(R.id.btnBack);
-        btnBack.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                CloseAll();
-                finish(); // Die.
-            }
-        });
     }
 
     private void CloseAll() {
