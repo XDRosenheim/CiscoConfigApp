ab9768662b6f82ab4e7effad3ed3b3eeff325bc1
Rosenheim <patrickrosenheim@gmail.com>
2016-05-31 11:42:12 +0200

    Popup.

--- app/src/main/java/app/ciscoconfig/dk/ciscoconfigapp/HomeActivity.java
+++ app/src/main/java/app/ciscoconfig/dk/ciscoconfigapp/HomeActivity.java
@@ -1,10 +1,15 @@
 package app.ciscoconfig.dk.ciscoconfigapp;
 
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.io.BufferedReader;
@@ -62,8 +67,33 @@ public class HomeActivity extends AppCompatActivity {
         btnSetMotd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                final String[] text = {null};
                 ConfigureCommandBlocks cmd = new ConfigureCommandBlocks();
-                cmd.setMOTD("This is your brand new MOTD!");
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(getApplicationContext());
+                builder.setTitle("Title");
+                View viewInflated = LayoutInflater.from(getApplicationContext()).inflate(R.layout.text_inpu_password, null);
+                final EditText input = (EditText) viewInflated.findViewById(R.id.input);
+                builder.setView(viewInflated);
+
+                builder.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String temp_text = input.getText().toString();
+                        text[0] = temp_text;
+                        dialog.dismiss();
+                    }
+                });
+                builder.setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+
+                builder.show();
+
+                cmd.setMOTD(text[0]);
                 for (String sendMe : cmd.array) {
                     Out.println(sendMe); // Send commands to device.
                 }
