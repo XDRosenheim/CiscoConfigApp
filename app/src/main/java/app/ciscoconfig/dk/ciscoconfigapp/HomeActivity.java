53480a644a48e6ebfb10cc676106c3697a0dea39
Rosenheim <patrickrosenheim@gmail.com>
2016-06-01 09:38:11 +0200

    Formatting.

--- app/src/main/java/app/ciscoconfig/dk/ciscoconfigapp/HomeActivity.java
+++ app/src/main/java/app/ciscoconfig/dk/ciscoconfigapp/HomeActivity.java
@@ -2,11 +2,8 @@ package app.ciscoconfig.dk.ciscoconfigapp;
 
 
 import android.app.Activity;
-import android.app.Dialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AlertDialog;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ScrollView;
