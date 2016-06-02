fe6fcbe8a5ebb96a55b18b31c26df302acfd3a32
Rosenheim <patrickrosenheim@gmail.com>
2016-06-02 12:09:12 +0200

    Switch > If

--- app/src/main/java/app/ciscoconfig/dk/ciscoconfigapp/HomeActivity.java
+++ app/src/main/java/app/ciscoconfig/dk/ciscoconfigapp/HomeActivity.java
@@ -1,6 +1,5 @@
 package app.ciscoconfig.dk.ciscoconfigapp;
 
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,7 +7,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ScrollView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -17,24 +15,19 @@ import java.net.Socket;
 
 public class HomeActivity extends Activity {
 
-    static final int Popup_SvarMotd = 1;
-    static final int Popup_SvarHostname = 1;
-
+    private static final int CodeMotd = 2;
+    private static final int CodeHostName = 3;
     Button btnBack, btnSetHostname, btnSetMotd;
     TextView txtConsole;
     ScrollView viewterminal;
     private Socket Sock;
     private PrintWriter Out;
     private BufferedReader In;
-    int CodeMotd = 2;
-    int CodeHostName = 3;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-
 
         Sock = Singleton.getSocket();
         Out = Singleton.getOut();
@@ -43,11 +36,8 @@ public class HomeActivity extends Activity {
         btnSetHostname = (Button) findViewById(R.id.btnSetHostname);
         btnSetMotd = (Button) findViewById(R.id.btnSetMotd);
         btnBack = (Button) findViewById(R.id.btnBack);
-
         viewterminal = (ScrollView) findViewById(R.id.viewterminal);
-
         txtConsole = (TextView) findViewById(R.id.txtConsole);
-
 
         Thread t1 = new Thread(new Runnable() {
             @Override
@@ -70,9 +60,6 @@ public class HomeActivity extends Activity {
         btnSetMotd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-
-
                 Intent GetPopupData = new Intent(HomeActivity.this, Pop.class);
                 startActivityForResult(GetPopupData, CodeMotd);
             }
@@ -82,10 +69,8 @@ public class HomeActivity extends Activity {
             @Override
             public void onClick(View v) {
                 ConfigureCommandBlocks cmd = new ConfigureCommandBlocks();
-
                 Intent GetPopupData = new Intent(HomeActivity.this, Pop.class);
                 startActivityForResult(GetPopupData, CodeHostName);
-
                 for (String sendMe : cmd.array) {
                     Out.println(sendMe); // Send commands to device.
                     txtConsole.setText(txtConsole.getText().toString() + "\n" + sendMe);
@@ -121,32 +106,27 @@ public class HomeActivity extends Activity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
         ConfigureCommandBlocks cmd = new ConfigureCommandBlocks();
-
-        if(requestCode==CodeHostName){
-
-            if(resultCode == RESULT_OK) {
-                String Svaret = data.getStringExtra("PopAnswer");
-
-                cmd.setHostName(Svaret);
-
+        switch (requestCode) {
+            case CodeHostName: {
+                if (resultCode == RESULT_OK) {
+                    String Svaret = data.getStringExtra("PopAnswer");
+                    cmd.setHostName(Svaret);
+                    for (String str : cmd.array) {
+                        Out.println(str);
+                    }
+                }
+            }
+            case CodeMotd: {
+                if (resultCode == RESULT_OK) {
+                    String Svaret = data.getStringExtra("PopAnswer");
+                    cmd.setMOTD(Svaret);
+                    for (String str : cmd.array) {
+                        Out.println(str);
+                    }
+                }
             }
         }
-
-        if(requestCode==CodeMotd){
-
-            if(resultCode == RESULT_OK) {
-                String Svaret = data.getStringExtra("PopAnswer");
-
-                cmd.setMOTD(Svaret);
-
-            }
-        }
-
-
-
     }
-
 }
 
