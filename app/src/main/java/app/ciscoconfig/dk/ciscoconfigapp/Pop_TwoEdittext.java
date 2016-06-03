076cf1a2ea4b4be71cbad680264f839117999da0
XDRosenheim <patrickrosenheim@gmail.com>
2016-06-03 10:47:40 +0200

    If you do this, at least...

--- app/src/main/java/app/ciscoconfig/dk/ciscoconfigapp/Pop_TwoEdittext.java
+++ app/src/main/java/app/ciscoconfig/dk/ciscoconfigapp/Pop_TwoEdittext.java
@@ -28,7 +28,7 @@ public class Pop_TwoEdittext extends Activity {
     public void BtnOK(View v){
         String[] PopUpAnswer = new String[0];
         PopUpAnswer[0] = FirstInput.getText().toString();
-        PopUpAnswer[1] = FirstInput.getText().toString();
+        PopUpAnswer[1] = SecondInput.getText().toString();
         Intent AnswerBack = new Intent();
         AnswerBack.putExtra("PopMultiAnswer", PopUpAnswer);
         setResult(RESULT_OK, AnswerBack);
