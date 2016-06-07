4e3e19de041114aa9bc426d5b824029e71652158
Math <Baskerwild@hotmail.dk>
2016-06-07 13:13:27 +0200

    Check på Hostname og banner

--- app/src/main/java/app/ciscoconfig/dk/ciscoconfigapp/HomeActivity.java
+++ app/src/main/java/app/ciscoconfig/dk/ciscoconfigapp/HomeActivity.java
@@ -145,7 +145,7 @@ public class HomeActivity extends Activity {
             case CodeHostName: {
                 if (resultCode == RESULT_OK) {
                     String AnswerFromPop = data.getStringExtra("PopAnswer");
-                    AnswerFromPop.replaceAll(" ","");
+                    AnswerFromPop.replaceAll("[-+.^:0-9 ]","");
                     cmd.setHostName(AnswerFromPop);
                     Toast.makeText(getApplicationContext(), "HOSTNAME was set.", Toast.LENGTH_SHORT).show();
                 }
